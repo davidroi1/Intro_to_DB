338a36f8e27ffcb5c64c2e81d63fdbe8affed63d
davidroi1 <vagba.belia1@gmail.com>
2025-07-26 11:20:52 +0000

    submission for task_5 file

---
+++ task_5.sql
@@ -0,0 +1,12 @@
+USE alx_book_store;
+
+INSERT INTO customer (
+    customer_name,
+    customer_email,
+    email,
+    address
+) VALUES (
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave'
+)
