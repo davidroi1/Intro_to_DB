b53d922db6e57c5bdb15577b6303806cf90f8769
davidroi1 <vagba.belia1@gmail.com>
2025-07-26 10:45:51 +0000

    submission for task_2 file

--- task_2.sql
+++ task_2.sql
@@ -1,36 +1,40 @@
 CREATE TABLE Books (
-    book_id INT PRIMARY_KEY,
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130),
-    author_id(Foreign Key (author_id) REFERENCES Authors(author_id)),
+    author_id INT,
     price DOUBLE,
-    publication_date DATE
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 
 CREATE TABLE Authors (
-    author_id INT PRIMARY_KEY,
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215)
 );
 
 
 CREATE TABLE Customers (
-    customer_id INT PRIMARY_KEY,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215),
-    email VARCHAR(215),
+    email VARCHAR(215) UNIQUE,
     address TEXT
 );
 
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY_KEY,
-    customer_id (Foreign Key (customer_id) REFERENCES Customers(customer_id)),
-    order_date DATE
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
 CREATE TABLE Order_Details (
-    orderdetail_id INT PRIMARY_KEY,
-    order_id (Foreign Key (order_id) REFERENCES Orders(order_id)),
-    book_id (Foreign Key (book_id) REFERENCES Books(book_id)),
+    orderdetail_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
